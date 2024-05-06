f5adab8039a4e06c5fb7e43f9df56bc7f2ff9a29
Miles Foret <milesforet@hotmail.com>
2024-05-06 19:21:13 -0400

    Sharepoint list working with data from UPS in test environment

--- Get_Tracking_Info.ps1
+++ Get_Tracking_Info.ps1
@@ -12,57 +12,93 @@ $query = @{
     returnMilestones= "false"
   }
 
-$shipment = "1Z3124Y81515962179"
+Function Call-Api($tracking_num){
 
-$url = "https://onlinetools.ups.com/api/track/v1/details/"+$shipment
+    $url = "https://onlinetools.ups.com/api/track/v1/details/"+$tracking_num
 
-$result = Invoke-RestMethod -Uri $url -Method Get -Headers $headers -Body $query
+    $data = @{
+        label_created=""
+        curr_status="N/A"
+        shipped_date="N/A"
+        last_scan="N/A"
+        last_scan_local="N/A"
+        deliv_date="N/A"
+        ship_to=""
+        reference=""
+        ship_service=""
+    }
 
-$last_scan = $result.trackResponse.shipment.package
+    $result = Invoke-RestMethod -Uri $url -Method Get -Headers $headers -Body $query
 
-Write-Output $last_scan
+    #Label Created
+    $data.label_created = $result.trackResponse.shipment.package.activity
+    $data.label_created = $result.trackResponse.shipment.package.activity[$label_created.Length-1].date
+    $data.label_created = [datetime]::parseexact($data.label_created, 'yyyyMMdd', $null).ToString('MM/dd/yyyy')
 
-#Label Created
-<# $label_created = $result.trackResponse.shipment.package.activity
-$label_created = $result.trackResponse.shipment.package.activity[$label_created.Length-1].date
-$label_created = [datetime]::parseexact($label_created, 'yyyyMMdd', $null).ToString('MM/dd/yyyy') #>
+    #Current Status
+    $data.curr_status = $result.trackResponse.shipment.package.currentStatus.description
 
-#Current Status
-#$curr_status = $result.trackResponse.shipment.package.currentStatus.description
+    if($data.curr_status -ne "Shipment Ready for UPS"){
 
-#Shipped Date
-<# $label_created = $result.trackResponse.shipment.package.activity
-$label_created = $result.trackResponse.shipment.package.activity[$label_created.Length-2].date
-$label_created = [datetime]::parseexact($label_created, 'yyyyMMdd', $null).ToString('MM/dd/yyyy') #>
+        #Shipped Date
+        $data.shipped_date = $result.trackResponse.shipment.package.activity
+        $data.shipped_date = $result.trackResponse.shipment.package.activity[$shipped_date.Length-2].date
+        $data.shipped_date = [datetime]::parseexact($data.shipped_date, 'yyyyMMdd', $null).ToString('MM/dd/yyyy')
 
-#Last Scan Date/Time
+        #Last Scan Date/Time
+        $last_scan = $result.trackResponse.shipment.package.activity[0].date
+        $last_scan = [datetime]::parseexact($last_scan, 'yyyyMMdd', $null).ToString('MM/dd/yyyy')
+        $last_time = $result.trackResponse.shipment.package.activity[0].time
+        $last_time = [datetime]::parseexact($last_time, 'HHmmss', $null).ToString('hh:mm tt')
+        $data.last_scan = "$last_scan $last_time"
+
+        #Last Scan Location
+        $data.last_scan_local = $result.trackResponse.shipment.package.activity[0].location.address.city
+        $data.last_scan_local = $data.last_scan_local+ ", "  + $result.trackResponse.shipment.package.activity[0].location.address.stateProvince
+    }
+
+    if($data.curr_status -eq "Delivered"){
+        #Delivery Date
+        $data.deliv_date = $result.trackResponse.shipment.package.deliveryDate.date
+        $data.deliv_date = [datetime]::parseexact($data.deliv_date, 'yyyyMMdd', $null).ToString('MM/dd/yyyy')
+    }
+
+    #Ship to
+    $to_address1 = $result.trackResponse.shipment.package.packageAddress[1].address.addressLine1
+    $to_address1 = $to_address1.Substring(0,$to_address1.Length-1)
+    $to_address2 = $result.trackResponse.shipment.package.packageAddress[1].address.addressLine2
+    $city = $result.trackResponse.shipment.package.packageAddress[1].address.city
+    $state = $result.trackResponse.shipment.package.packageAddress[1].address.stateProvince
+    $zip = $result.trackResponse.shipment.package.packageAddress[1].address.postalCode
+    $data.ship_to = ""
+
+    if($to_address2.Length -eq 0){
+        $data.ship_to = "$to_address1 $city, $state $zip"
+    }else{
+        $data.ship_to = "$to_address1 $to_address2 $city, $state $zip"
+    }
+
+    #Reference
+    $data.reference = $result.trackResponse.shipment.package.referenceNumber[0].number
+
+    #Ship Service
+    $data.ship_service = $result.trackResponse.shipment.package.service.description
+
+    Return $data
+}
 
 
-#Last Scan Location
+#CONNECT TO SHAREPOINT
+Connect-PnPOnline -url "https://70rspw.sharepoint.com/sites/Testing" -interactive
 
+#GET SHAREPOINT LIST
+$info = get-pnplistitem -list tracking
 
+for($i=1; $i -le $info.Length; $i++){
+    $id = $info[$i-1].Id
+    $new_data = Call-Api($info[$i-1]["Title"])
 
-
-#Delivery Date
-#$deliv_date = $result.trackResponse.shipment.package.deliveryDate.date
-#$deliv_date = [datetime]::parseexact($deliv_date, 'yyyyMMdd', $null).ToString('MM/dd/yyyy')
-
-#Ship to
-<# $to_address1 = $result.trackResponse.shipment.package.packageAddress[1].address.addressLine1
-$to_address1 = $to_address1.Substring(0,$to_address1.Length-1)
-$to_address2 = $result.trackResponse.shipment.package.packageAddress[1].address.addressLine2
-$city = $result.trackResponse.shipment.package.packageAddress[1].address.city
-$state = $result.trackResponse.shipment.package.packageAddress[1].address.stateProvince
-$zip = $result.trackResponse.shipment.package.packageAddress[1].address.postalCode
-$ship_to = ""
-if($to_address2.Length -eq 0){
-    $ship_to = "$to_address1 $city, $state $zip"
-}else{
-    $ship_to = "$to_address1 $to_address2 $city, $state $zip"
-} #>
-
-#Reference
-#$reference = $result.trackResponse.shipment.package.referenceNumber[0].number
-
-#Ship Service
-#ship_service = $result.trackResponse.shipment.package.service.description
+    Set-PnPListItem -List "Tracking" -Identity $id -Values @{"LabelCreated" = $new_data.label_created; "Status"=$new_data.curr_status; "ShippedDate"=$new_data.shipped_date;
+    "LastScanDate_x002f_Time"=$new_data.last_scan; "LastScanLocation"=$new_data.last_scan_local;  "ShippingTo"=$new_data.ship_to; "DeliveryDate"=$new_data.deliv_date; 
+    "Reference"=$new_data.reference; "ShipService"=$new_data.ship_service}
+}
